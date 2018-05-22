a661b80d9e15fccec7608d1bbb310dc7e04dccd2
iamlucasalves <lukasalvespod1@gmail.com>
2018-05-22 11:03:43 -0300

    Dados das textboxs convertidos para Double

--- ProjetoEstatistica/Form1.cs
+++ ProjetoEstatistica/Form1.cs
@@ -19,7 +19,14 @@ namespace ProjetoEstatistica
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-              
+            double[] vetorDados = new double[7];
+            vetorDados[0] = Convert.ToDouble(txtN1.Text);
+            vetorDados[1] = Convert.ToDouble(txtN2.Text);
+            vetorDados[2] = Convert.ToDouble(txtN3.Text);
+            vetorDados[3] = Convert.ToDouble(txtN4.Text);
+            vetorDados[4] = Convert.ToDouble(txtN5.Text);
+            vetorDados[5] = Convert.ToDouble(txtN6.Text);
+            vetorDados[6] = Convert.ToDouble(txtN7.Text);
         }
     }
 }
